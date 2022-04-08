882f2c11d1d3326be293519c03e74844572e4fde
MastroSo <alessandrogiovannisodano@gmail.com>
2022-04-08 12:43:58 +0200

    Aggiungere i file di progetto.

---
+++ csharp-geometria/Program.cs
@@ -0,0 +1,27 @@
+﻿using Geometria;
+
+Console.WriteLine("Ciao bell!\n");
+
+//Primo Rettangolo
+Console.WriteLine("Inserire la base del rettangolo: ");
+int larghezza = Convert.ToInt32(Console.ReadLine()); //larghezza = base
+Console.WriteLine("Inserire l'altezza del rettangolo: ");
+int altezza = Convert.ToInt32(Console.ReadLine());
+
+Rettangolo rettangolo = new Rettangolo(larghezza, altezza);
+
+rettangolo.stampaRettangolo();
+
+rettangolo.Disegna();
+
+//Secondo Rettangolo
+Console.WriteLine("Inserire la base del rettangolo: ");
+larghezza = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Inserire l'altezza del rettangolo: ");
+altezza = Convert.ToInt32(Console.ReadLine());
+Rettangolo nuovoRettangolo = new Rettangolo(larghezza, altezza);
+
+nuovoRettangolo.stampaRettangolo();
+
+nuovoRettangolo.Disegna();
+
---
+++ csharp-geometria/Rettangolo.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpGeometria
+{
+    internal class Rettangolo
+    {
+        private int baseRettangolo; //attributi
+        private int altezzaRettangolo;
+
+        public Rettangolo(int x, int y) // x = base, y = altezza
+        { //costruttore
+            this.baseRettangolo = x;
+            this.altezzaRettangolo = y;
+        }
+
+        private int calcolaArea() //metodo per calcolare l'area
+        {
+            return baseRettangolo * altezzaRettangolo;
+        }
+
+        private int calcolaPerimetro() //metodo per calcolare il perimetro
+        {
+            return (baseRettangolo + altezzaRettangolo) * 2;
+        }
+
+        public void stampaRettangolo()
+        {
+            Console.WriteLine("—— Rettangolo1 ——");
+            Console.WriteLine("Base: " + baseRettangolo + "cm");
+            Console.WriteLine("Altezza: " + altezzaRettangolo + "cm");
+            Console.WriteLine("Perimetro: " + calcolaPerimetro() + "cm");
+            Console.WriteLine("Area: " + calcolaArea() + "cm2");
+            Console.WriteLine("-----------------");
+        }
+
+        public void Disegna()
+        {
+            string unitaX = "--";
+            string unitaY = "|";
+
+            Console.Write(" ");
+            for (int i = 0; i < baseRettangolo - 1; i++) //lato superiore
+            {
+                Console.Write(unitaX);
+            }
+            Console.WriteLine(unitaX);
+            for (int i = 0; i < altezzaRettangolo; i++)
+            {
+                Console.Write(unitaY);
+                for (int j = 0; j < baseRettangolo; j++) //lati laterali
+                {
+                    Console.Write("  ");
+                }
+                Console.WriteLine(unitaY);
+            }
+            Console.Write(" ");
+            for (int i = 0; i < baseRettangolo - 1; i++) //lato inferiore
+            {
+                Console.Write(unitaX);
+            }
+            Console.WriteLine(unitaX);
+        }
+    }
+}
